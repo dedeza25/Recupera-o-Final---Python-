51d1ce2753a7921b8a4aed9ade9ac2aada22185e
dedeza25 <andrezanascimento@colegiopaulodetarso.com.br>
2025-12-07 16:13:40 +0000

    8

--- ex8.py
+++ ex8.py
@@ -1,1 +1,10 @@
-gggg
+qtd = int(input("Quantos números? "))
+numeros = []
+
+for i in range(qtd):
+    n = int(input("Digite um número: "))
+    numeros.append(n)
+
+print("Maior:", max(numeros))
+print("Menor:", min(numeros))
+print("Quantidade:", len(numeros))
